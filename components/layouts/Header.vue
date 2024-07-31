5363b1fcdf0fd18e5b051b8e4b9aec1402f240c5
rptec <108799559+rptecs@users.noreply.github.com>
2024-07-31 22:25:15 +0800

    Update Header.vue

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -15,7 +15,7 @@ const showMenu = ref(false)
         <div class="flex items-center justify-start w-1/4 h-full pr-4">
           <a
             href="/"
-            title="Sink"
+            title="2024"
             class="flex items-center py-4 space-x-2 text-xl font-black text-gray-900 dark:text-gray-100 md:py-0"
           >
             <span
@@ -27,7 +27,7 @@ const showMenu = ref(false)
                 class="w-full h-full rounded-full"
               >
             </span>
-            <span class="mx-2">Sink</span>
+            <span class="mx-2">2024</span>
           </a>
         </div>
 
@@ -41,7 +41,7 @@ const showMenu = ref(false)
           >
             <a
               href="/"
-              title="Sink"
+              title="2024"
               class="inline-flex items-center w-auto h-16 px-4 text-xl font-black leading-none text-gray-900 dark:text-gray-100 md:hidden"
             >
               <span
@@ -53,7 +53,7 @@ const showMenu = ref(false)
                   class="w-full h-full rounded-full"
                 >
               </span>
-              <span class="mx-2">Sink</span>
+              <span class="mx-2">2024</span>
             </a>
             <div class="w-full mx-4" />
             <div
@@ -62,12 +62,12 @@ const showMenu = ref(false)
               <a
                 class="w-full px-6 py-2 mr-0 text-gray-700 cursor-pointer dark:text-gray-300 md:px-3 md:mr-2 lg:mr-3 md:w-auto"
                 href="/dashboard"
-                title="Sink Dashboard"
+                title="2024 Dashboard"
               >Dashboard</a>
               <a
-                href="https://github.com/ccbikai/sink"
+                href="#2024"
                 target="_blank"
-                title="Github"
+                title="2024"
                 class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:px-3 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
               >
                 <GitHubIcon
